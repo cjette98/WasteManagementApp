759ec17cab813000ba8d1965f2eb7800e672f085
Jette Kerz Castillo <j.k.castillo@prospire.io>
2023-01-29 19:51:26 +0800

    Firebase setup and template

--- customerMobileApp/App.js
+++ customerMobileApp/App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { auth,app} from './src/config/firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
+import { collection,addDoc } from "firebase/firestore"; 
+import { db } from './src/config/firebase';
 
-export default function App() {
+
+
+
+export default function App() { 
+
+  const regNewUser = async() => {
+    try {
+     await createUserWithEmailAndPassword(auth,'nijel88562@ekcsoft.com', 'cust@123')
+    await addDoc(collection(db, "Users"), {
+      name: "Abel",
+      email: "nijel88562@ekcsodf.com",
+      contact_number: "09387950432"
+    });
+    
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const loginUser = async () => {
+    try {
+    const resp =  await signInWithEmailAndPassword(auth, 'nijel88562@ekcsoft.com', 'cus@123');
+    console.log(JSON.stringify(resp))
+    } catch (error) {
+     console.log('INI',error)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={regNewUser}>LOGIN</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ customerMobileApp/app.config.js
@@ -0,0 +1,50 @@
+import "dotenv/config";
+export default {
+  expo: {
+    name: "customerMobileApp",
+    slug: "customerMobileApp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.waste-management-app",
+      googleServicesFile: "./GoogleService-Info.plist",
+    },
+    android: {
+      googleServicesFile: "./google-services.json",
+      package: "com.waste_management_app",
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      eas: {
+        projectId: "46f3034a-78bd-42f9-8962-8f28c706b8e1",
+      },
+    },
+  }
+}
